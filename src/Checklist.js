d2c2fe65edace2b11422ac491a4e927fdf8b5e95
ManuelARobles <robles.manuel.4796@gmail.com>
2023-08-04 19:37:39 -0500

    cleaning up

--- src/Checklist.js
+++ src/Checklist.js
@@ -4,6 +4,8 @@ import './styles.scss';
 
 function Checklist() {
   const [tasks, setTasks] = useState([]);
+  const [newTaskName, setNewTaskName] = useState('');
+  const [newTaskStatus, setNewTaskStatus] = useState('not-done');
 
   useEffect(() => {
     fetchTasks();
@@ -20,7 +22,7 @@ function Checklist() {
 
   const handleStatusChange = async (index, newStatus) => {
     try {
-      await axios.put(`https://checklistproject-31482afaf05a.herokuapp.com/api/tasks${index}`, { status: newStatus });
+      await axios.put(`https://checklistproject-31482afaf05a.herokuapp.com/api/tasks/${index}`, { status: newStatus });
       const updatedTasks = tasks.map((task, i) =>
         i === index ? { ...task, taskStatus: newStatus } : task
       );
@@ -30,9 +32,40 @@ function Checklist() {
     }
   };
 
+  const handleAddTask = async () => {
+    try {
+      await axios.post('https://checklistproject-31482afaf05a.herokuapp.com/api/tasks', {
+        taskName: newTaskName,
+        taskStatus: newTaskStatus,
+      });
+      setNewTaskName('');
+      setNewTaskStatus('not-done');
+      fetchTasks();
+    } catch (error) {
+      console.error('Error adding new task:', error);
+    }
+  };
+
   return (
     <div className="container">
       <h1>Checklist</h1>
+      <form>
+        <input
+          type="text"
+          value={newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)}
+          placeholder="Enter new task name"
+        />
+        <select
+          value={newTaskStatus}
+          onChange={(e) => setNewTaskStatus(e.target.value)}
+        >
+          <option value="not-done">Not Done</option>
+          <option value="done">Done</option>
+          <option value="N/A">N/A</option>
+        </select>
+        <button type="button" onClick={handleAddTask}>Add Task</button>
+      </form>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
